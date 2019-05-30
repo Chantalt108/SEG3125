4d9a073c70509cc3f3c66c32b467d43a7abf6876
Eleanor Rumsey <ellierums@gmail.com>
2019-05-30 12:18:51 -0400

    Made cities, meal types, and cuisines arrays in js

---
+++ Lab1/Welcome/welcome.js
@@ -0,0 +1,33 @@
+
+let cities = ['Ottawa', 'Toronto', 'Kingston', 'Montreal'];
+let mealTypes = ['Breakfast', 'Lunch', 'Dinner', 'Snacks', 'Late Night', 'Dessert'];
+let cuisines = ['Indian', 'Chinese', 'Italian', 'Burgers', 'Vegan', 'Thai'];
+
+
+var citySelect = document.getElementById('city-select').options;
+cities.forEach(city =>
+    citySelect.add(
+        new Option(city)
+    )
+);
+
+mealTypes.forEach(mealType => {
+    var b = document.createElement('button');
+    b.setAttribute('content', mealType);
+    b.setAttribute('class', 'subsection');
+    b.innerHTML = mealType;
+
+    var wrapper = document.getElementById('mealTypes');
+    wrapper.appendChild(b);
+});
+
+cuisines.forEach(cuisine => {
+    var b = document.createElement('button');
+    b.setAttribute('content', cuisine);
+    b.setAttribute('class', 'subsection');
+    b.innerHTML = cuisine;
+
+    var wrapper = document.getElementById('cuisines');
+    wrapper.appendChild(b);
+});
+
