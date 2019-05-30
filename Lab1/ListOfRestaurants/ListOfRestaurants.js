cfe7c3e371b551e779623a137d504a289f3bc707
Chantal Tseung <chantal_tseung@hotmail.com>
2019-05-30 12:37:53 -0400

    Javascript for navigating ListofRestaurants.html to Menu.html

---
+++ Lab1/ListOfRestaurants/ListOfRestaurants.js
@@ -0,0 +1,7 @@
+var isClicked = false;
+var restId = "";
+
+function navMenu(restId) {
+    isClicked = true;
+    window.location.href = "/Users/chant/OneDrive/Documents/Notes/SEG3125/Labs/Lab1/Menu/Menu.html" + '#' + restId;      
+}
---
+++ Lab1/Menu/Menu.js
@@ -0,0 +1,7 @@
+var restId = window.location.hash.substr(1);
+
+function loadMenu() {
+    if(restId == "rest1"){
+        //Load corresponding menu items based on restId
+    }
+}
