1363e447942245e73120e2fde7e3b8b9d7e97179
Eleanor Rumsey <ellierums@gmail.com>
2019-06-06 19:29:31 -0400

    Revert "temporary changes"
    This reverts commit 4fa6ad9a5ce40afdd73243184e832b6148036141.

--- Lab1/NavBar/nav.js
+++ Lab1/NavBar/nav.js
@@ -1,10 +1,3 @@
-document.getElementById("search-button").addEventListener("click", () => {
-    console.log("Hello");
-});
-
-console.log("hello");
-
-
 function search() {
     console.log(document.getElementById("search-bar").value);
 }
