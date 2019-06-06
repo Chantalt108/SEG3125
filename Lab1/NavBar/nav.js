4fa6ad9a5ce40afdd73243184e832b6148036141
Eleanor Rumsey <ellierums@gmail.com>
2019-06-06 19:29:22 -0400

    temporary changes

--- Lab1/NavBar/nav.js
+++ Lab1/NavBar/nav.js
@@ -1,3 +1,10 @@
+document.getElementById("search-button").addEventListener("click", () => {
+    console.log("Hello");
+});
+
+console.log("hello");
+
+
 function search() {
     console.log(document.getElementById("search-bar").value);
 }
